4366e87837531173abd1c0f0a2a99980a0f89a47
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-04-06 11:43:01 -0500

    finished exercise

--- planets.py
+++ planets.py
@@ -1,1 +1,20 @@
 planet_list = ["Mercury", "Mars"]
+planet_list.append("Jupiter")
+planet_list.append("Saturn")
+planet_list.extend(['Uranus', 'Neptune'])
+planet_list.insert(1, 'Venus')
+planet_list.insert(2, 'Earth')
+planet_list.append('Pluto')
+rocky_planets = planet_list[0:4]
+del planet_list[8]
+
+print(planet_list)
+print(rocky_planets)
+
+spacecraft_visit = [('Merri', 'Mercury'), ('Lady Gaga', 'Venus'), ('Cassini', 'Saturn')]
+
+
+for planet in planet_list:
+	for craft in spacecraft_visit:
+		if planet in craft:
+			print(planet+" has been visited by "+str(craft[0]))
